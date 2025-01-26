8787a21cc66e77288eaadc72bd18a14126e52f80
Daryl Kevin <dkevin77@icloud.com>
2025-01-26 22:38:36 +0800

    layouting

--- app/page.tsx
+++ app/page.tsx
@@ -24,14 +24,14 @@ export default function Home() {
   return (
     <ThemeProvider>
       <div className="mx-auto max-w-screen-md h-[100vh]">
-        <div className="mx-auto my-auto h-full flex flex-col justify-center px-2 xs:max-w-screen-xs md:max-w-screen-md">
+        <div className="mx-auto my-auto h-full flex flex-col justify-center px-2 xs:max-w-screen-xs md:max-w-screen-sm lg:max-w-screen-md">
           <div className="flex flex-col gap-2 h-fit rounded-xl p-4 shadow-xl dark:shadow-lg dark:shadow-green-400/75">
             <header className="md:hidden">
               <Navigation />
             </header>
 
-            <main className="grid md:grid-cols-2 xl:grid-cols-3">
-              <div className="grid xl:col-span-2">
+            <main className="grid md:grid-cols-3 lg:grid-cols-4">
+              <div className="grid md:col-span-2 lg:col-span-3">
                 <Hero />
                 <Profiles />
               </div>
