5eae034ad2a02966b9cc0f316f762ab18d7b73d3
Daryl Kevin <dkevin77@icloud.com>
2025-02-09 13:21:29 +0800

    adding popping motion and sticky position to cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { useEffect, useState } from "react";
+import { motion, useAnimationControls } from "motion/react";
 // xs: 320
 // sm: 640
 // md: 768
@@ -6,48 +9,33 @@ import { useEffect, useState } from "react";
 // xl: 1280
 // 2xl: 1536
 
-import { navs, hero } from "./lib/definitions"
-import { M_PLUS_1 } from "next/font/google";
-import Hero from "./ui/Hero";
-import Profiles from "./ui/Profiles";
-import Navigation from "./ui/Navigation";
 import { ThemeProvider } from "./ui/contexts/ThemeContext";
-import Footer from "./ui/Footer";
-
+import TitleCard from "./ui/TitleCard";
+import AboutCard from "./ui/AboutCard";
+import ExperienceCard from "./ui/ExperienceCard";
 // mobile-phones: 320px-480px
 // tablets: 768px-1024px
 // laptops: 1024px-1440px
 // monitors: 1440px-2560px
 
 export default function Home() {
+  const isMediumScreen = window.matchMedia("(max-width: 768px)").matches;
 
   return (
-    <ThemeProvider>
-      <div className="mx-auto max-w-screen-md h-[100vh]">
-        <div className="mx-auto my-auto h-full flex flex-col justify-center px-2 xs:max-w-screen-xs md:max-w-screen-sm lg:max-w-screen-md">
-          <div className="flex flex-col gap-2 h-fit rounded-xl p-4 shadow-xl dark:shadow-lg dark:shadow-green-400/75">
-            <header className="md:hidden">
-              <Navigation />
-            </header>
-
-            <main className="grid md:grid-cols-3 lg:grid-cols-4">
-              <div className="grid md:col-span-2 lg:col-span-3">
-                <Hero />
-                <Profiles />
-              </div>
-
-              <div className="hidden md:block">
-                <Navigation shortHand={false} flex={false}/>
-              </div>
-            </main>
-          </div>
-
-          <footer>
-              <Footer />
-          </footer>
-
-        </div>
-      </div>
-    </ThemeProvider>
+    <motion.div 
+    variants={{
+      pop: {
+        scale: [0.5, 1],
+        opacity: [0, 1],
+      },
+    }}
+    initial="initial"
+    animate={isMediumScreen ? 'pop' : 'initial'}>
+      <ThemeProvider>
+        <TitleCard />
+        <AboutCard />
+        <ExperienceCard />
+      </ThemeProvider>
+    </motion.div>
   );
 }
--- app/ui/Navigation.tsx
+++ app/ui/Navigation.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from 'react'
+import { motion } from 'motion/react'
 import { navs } from '../lib/definitions'
 
 interface Navigation {
@@ -10,8 +13,12 @@ const Navigation = ( { shortHand = true, flex = true } : Navigation ) => {
   return (
     <nav className={`font-normal ${flex ? "flex justify-between gap-2 pb-4" : "p-4"}`}>
         {navs.map((nav, i) => 
-            <div key={i} className='hover:font-semibold hover:cursor-pointer hover:text-lg transition-all'>
-                {shortHand ? nav.shortPart : nav.longPart}
+            <div key={i} className=''>
+                <motion.p
+                initial={{ x: 0 }}
+                whileHover={{ x: 10, scale: 1.05 }}>
+                  {shortHand ? nav.shortPart : nav.longPart}
+                </motion.p>
             </div>
         )}
     </nav>
