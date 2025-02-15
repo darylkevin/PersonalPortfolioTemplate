9f647f6c9c135221bf60e741c9daddd30f8150c2
Daryl Kevin <dkevin77@icloud.com>
2025-02-15 11:28:17 +0800

    added hover effects and link

--- app/ui/cards/desktop/EducationCardDesktop.tsx
+++ app/ui/cards/desktop/EducationCardDesktop.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { education } from "@/app/lib/definitions";
 
-import Hero from "../../Hero";
-import Profiles from "../../Profiles";
-import Footer from "../../Footer";
-
 const EducationCardDesktop = () => {
+
+  const [ educationHovered, setEducationHovered ] = useState(null);
+
   return (
     <div className="flex h-[80vh] flex-col justify-between gap-8 lg:h-[60vh]">
       <header className="text-4xl font-bold">EDUCATION HISTORY</header>
@@ -15,8 +14,8 @@ const EducationCardDesktop = () => {
       <hr />
 
       <section className="flex flex-col gap-4 overflow-auto">
-        {education.map((education, index) => (
-          <div key={index}>
+        {education.map((education, i) => (
+          <div key={i} className={`pt-8 pr-4 transition-all ${educationHovered !== null && (i === educationHovered ? 'cursor-pointer' : 'opacity-30')}`}  onMouseEnter={() => setEducationHovered(i)} onMouseLeave={() => setEducationHovered(null)}>
             <img
               className="mb-2 h-[80px] w-full rounded-md object-cover shadow-lg lg:h-[100px]"
               src="/education/placeholder.png"
--- app/ui/cards/desktop/ExperienceCardDesktop.tsx
+++ app/ui/cards/desktop/ExperienceCardDesktop.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { experiences } from "@/app/lib/definitions";
 
-import Hero from "../../Hero";
-import Profiles from "../../Profiles";
-import Footer from "../../Footer";
 
 const ExperienceCardDesktop = () => {
+
+  const [ experienceHovered, setExperienceHovered ] = useState(null);
+
   return (
     <div className="flex h-[80vh] flex-col justify-between gap-8 lg:h-[50vh]">
       <header className="text-4xl font-bold">PROFESSIONAL EXPERIENCES</header>
@@ -16,12 +16,26 @@ const ExperienceCardDesktop = () => {
 
       <section className="overflow-auto">
         {experiences.map((experience, i) => (
-          <div className="grid grid-cols-4 gap-8 pt-2 lg:grid-cols-5" key={i}>
+          <div className={`grid grid-cols-4 gap-8 pt-8 pr-4 lg:grid-cols-5 transition-all ${experienceHovered !== null && (i === experienceHovered ? 'cursor-pointer' : 'opacity-30')}`} key={i} onMouseEnter={() => setExperienceHovered(i)} onMouseLeave={() => setExperienceHovered(null)}>
             <p className="opacity-50">{experience.period}</p>
             <div className="col-span-3 lg:col-span-4">
+              <div className="flex justify-between">
               <span className="font-semibold">
                 {experience.title} · {experience.company}
               </span>
+              {/* <svg 
+              className={`text-black dark:text-white transition-all ${i === experienceHovered ? 'size-5' : 'pt-1 pr-1 size-5'}`} 
+              viewBox="0 0 1024 1024" 
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path fill="currentColor" d="M768 256H353.6a32 32 0 1 1 0-64H800a32 32 0 0 1 32 32v448a32 32 0 0 1-64 0V256z"/>
+              <path fill="currentColor" d="M777.344 201.344a32 32 0 0 1 45.312 45.312l-544 544a32 32 0 0 1-45.312-45.312l544-544z"/>
+            </svg> */}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`transition-all ${i === experienceHovered ? 'size-5' : 'size-3'}`}>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+              </svg>
+
+              </div>
               <p className="pt-2 text-justify font-normal opacity-50">
                 {experience.description}
               </p>
