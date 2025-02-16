947d8b8a1bf2f237477ac930526688cd59e3a6ca
Daryl Kevin <dkevin77@icloud.com>
2025-02-16 17:43:47 +0800

    edited footer

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="w-full mt-8 text-left text-[8px] italic flex justify-between">
       <div className="opacity-50">
-      <p> — Made with NextJS, TailwindCSS, Motion and &lt;3</p>
+      <p> — Made with NextJS, TailwindCSS and Motion</p>
       <p>
         © {new Date().getFullYear()}, {footer.text}
       </p>
