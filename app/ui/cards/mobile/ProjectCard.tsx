640fd841a623f3b1ba328316b3ca0c9658638849
Daryl Kevin <dkevin77@icloud.com>
2025-02-16 18:27:46 +0800

    added link icon on mobile view

--- app/ui/cards/mobile/ProjectCard.tsx
+++ app/ui/cards/mobile/ProjectCard.tsx
@@ -22,7 +22,23 @@ const ProjectCard = () => {
                 height={80}
               />
               <div className="col-span-2 flex flex-col">
-                <span className="font-semibold">{project.title}</span>
+                <div className="flex gap-2 font-semibold">
+                  <span>{project.title}</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-3 pr-1"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                    />
+                  </svg>
+                </div>
                 <div>
                   <p className="pt-2 text-justify font-normal opacity-50">
                     {project.description}
